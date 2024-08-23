745891941ad11876728cda5ce40161c4cdec3c25
Bert Ramirez <13988480+bertrmz@users.noreply.github.com>
2024-08-23 08:25:12 -0500

    add PassageFlex (#2)
    * start defining lib
    
    * use smaller structs for app- & user-info
    
    * allow setting server_url
    
    * add test suite
    
    * fix behavior in crate
    
    * update tests
    
    * map to custom crate error
    
    * rm intos
    
    * prefix Passage-Version header
    
    * const server url
    
    * all in one file
    
    * assert returned error
    
    * rm case handling that won't be hit
    
    * rename error file
    
    * separate tests to mod

---
+++ src/error.rs
@@ -0,0 +1,173 @@
+use crate::Error;
+use openapi::apis::{apps_api, authenticate_api, transactions_api, user_devices_api, users_api};
+
+// This function converts an openapi error into a crate error
+fn convert_error<Src>(error: openapi::apis::Error<Src>, map_fn: fn(Src) -> Error) -> Error {
+    match error {
+        openapi::apis::Error::Reqwest(e) => Error::Reqwest(e), // Forward the reqwest error directly
+        openapi::apis::Error::Serde(e) => Error::Serde(e),     // Forward the serde error directly
+        openapi::apis::Error::Io(e) => Error::Io(e),           // Forward the I/O error directly
+        openapi::apis::Error::ResponseError(response) => match response.entity {
+            Some(entity) => map_fn(entity),
+            None => Error::Other(response.content),
+        },
+    }
+}
+
+impl From<openapi::apis::Error<apps_api::GetAppError>> for Error {
+    fn from(e: openapi::apis::Error<apps_api::GetAppError>) -> Self {
+        convert_error(e, |e| match e {
+            apps_api::GetAppError::Status401(model) => model.into(),
+            apps_api::GetAppError::Status404(model) => model.into(),
+            apps_api::GetAppError::Status500(model) => model.into(),
+            apps_api::GetAppError::UnknownValue(v) => Error::Other(v.to_string()),
+        })
+    }
+}
+
+impl From<openapi::apis::Error<transactions_api::CreateRegisterTransactionError>> for Error {
+    fn from(e: openapi::apis::Error<transactions_api::CreateRegisterTransactionError>) -> Self {
+        convert_error(e, |e| match e {
+            transactions_api::CreateRegisterTransactionError::Status400(model) => model.into(),
+            transactions_api::CreateRegisterTransactionError::Status401(model) => model.into(),
+            transactions_api::CreateRegisterTransactionError::Status403(model) => model.into(),
+            transactions_api::CreateRegisterTransactionError::Status404(model) => model.into(),
+            transactions_api::CreateRegisterTransactionError::Status500(model) => model.into(),
+            transactions_api::CreateRegisterTransactionError::UnknownValue(v) => {
+                Error::Other(v.to_string())
+            }
+        })
+    }
+}
+
+impl From<openapi::apis::Error<transactions_api::CreateAuthenticateTransactionError>> for Error {
+    fn from(e: openapi::apis::Error<transactions_api::CreateAuthenticateTransactionError>) -> Self {
+        convert_error(e, |e| match e {
+            transactions_api::CreateAuthenticateTransactionError::Status400(model) => model.into(),
+            transactions_api::CreateAuthenticateTransactionError::Status401(model) => model.into(),
+            transactions_api::CreateAuthenticateTransactionError::Status403(model) => model.into(),
+            transactions_api::CreateAuthenticateTransactionError::Status404(model) => model.into(),
+            transactions_api::CreateAuthenticateTransactionError::Status409(model) => model.into(),
+            transactions_api::CreateAuthenticateTransactionError::Status500(model) => model.into(),
+            transactions_api::CreateAuthenticateTransactionError::UnknownValue(v) => {
+                Error::Other(v.to_string())
+            }
+        })
+    }
+}
+
+impl From<openapi::apis::Error<authenticate_api::AuthenticateVerifyNonceError>> for Error {
+    fn from(e: openapi::apis::Error<authenticate_api::AuthenticateVerifyNonceError>) -> Self {
+        convert_error(e, |e| match e {
+            authenticate_api::AuthenticateVerifyNonceError::Status400(model) => model.into(),
+            authenticate_api::AuthenticateVerifyNonceError::Status401(model) => model.into(),
+            authenticate_api::AuthenticateVerifyNonceError::Status403(model) => model.into(),
+            authenticate_api::AuthenticateVerifyNonceError::Status404(model) => model.into(),
+            authenticate_api::AuthenticateVerifyNonceError::Status500(model) => model.into(),
+            authenticate_api::AuthenticateVerifyNonceError::UnknownValue(v) => {
+                Error::Other(v.to_string())
+            }
+        })
+    }
+}
+
+impl From<openapi::apis::Error<users_api::ListPaginatedUsersError>> for Error {
+    fn from(e: openapi::apis::Error<users_api::ListPaginatedUsersError>) -> Self {
+        convert_error(e, |e| match e {
+            users_api::ListPaginatedUsersError::Status400(model) => model.into(),
+            users_api::ListPaginatedUsersError::Status401(model) => model.into(),
+            users_api::ListPaginatedUsersError::Status404(model) => model.into(),
+            users_api::ListPaginatedUsersError::Status500(model) => model.into(),
+            users_api::ListPaginatedUsersError::UnknownValue(v) => Error::Other(v.to_string()),
+        })
+    }
+}
+
+impl From<openapi::apis::Error<users_api::GetUserError>> for Error {
+    fn from(e: openapi::apis::Error<users_api::GetUserError>) -> Self {
+        convert_error(e, |e| match e {
+            users_api::GetUserError::Status401(model) => model.into(),
+            users_api::GetUserError::Status404(model) => model.into(),
+            users_api::GetUserError::Status500(model) => model.into(),
+            users_api::GetUserError::UnknownValue(v) => Error::Other(v.to_string()),
+        })
+    }
+}
+
+impl From<openapi::apis::Error<user_devices_api::ListUserDevicesError>> for Error {
+    fn from(e: openapi::apis::Error<user_devices_api::ListUserDevicesError>) -> Self {
+        convert_error(e, |e| match e {
+            user_devices_api::ListUserDevicesError::Status401(model) => model.into(),
+            user_devices_api::ListUserDevicesError::Status404(model) => model.into(),
+            user_devices_api::ListUserDevicesError::Status500(model) => model.into(),
+            user_devices_api::ListUserDevicesError::UnknownValue(v) => Error::Other(v.to_string()),
+        })
+    }
+}
+
+impl From<openapi::apis::Error<user_devices_api::DeleteUserDevicesError>> for Error {
+    fn from(e: openapi::apis::Error<user_devices_api::DeleteUserDevicesError>) -> Self {
+        convert_error(e, |e| match e {
+            user_devices_api::DeleteUserDevicesError::Status401(model) => model.into(),
+            user_devices_api::DeleteUserDevicesError::Status404(model) => model.into(),
+            user_devices_api::DeleteUserDevicesError::Status500(model) => model.into(),
+            user_devices_api::DeleteUserDevicesError::UnknownValue(v) => {
+                Error::Other(v.to_string())
+            }
+        })
+    }
+}
+
+impl From<openapi::models::model_401_error::Model401Error> for Error {
+    fn from(e: openapi::models::model_401_error::Model401Error) -> Self {
+        match e.code {
+            openapi::models::model_401_error::Code::AccessToken => Error::InvalidAccessToken,
+            openapi::models::model_401_error::Code::Nonce => Error::InvalidNonce,
+        }
+    }
+}
+
+impl From<openapi::models::model_400_error::Model400Error> for Error {
+    fn from(e: openapi::models::model_400_error::Model400Error) -> Self {
+        match e.code {
+            openapi::models::model_400_error::Code::InvalidRequest => Error::InvalidRequest,
+            _ => Error::Other(e.error),
+        }
+    }
+}
+
+impl From<openapi::models::model_403_error::Model403Error> for Error {
+    fn from(e: openapi::models::model_403_error::Model403Error) -> Self {
+        match e.code {
+            openapi::models::model_403_error::Code::OperationNotAllowed => {
+                Error::OperationNotAllowed
+            }
+            _ => Error::Other(e.error),
+        }
+    }
+}
+
+impl From<openapi::models::model_404_error::Model404Error> for Error {
+    fn from(e: openapi::models::model_404_error::Model404Error) -> Self {
+        match e.code {
+            openapi::models::model_404_error::Code::DeviceNotFound => Error::DeviceNotFound,
+            openapi::models::model_404_error::Code::UserNotFound => Error::UserNotFound,
+            _ => Error::Other(e.error),
+        }
+    }
+}
+
+impl From<openapi::models::model_409_error::Model409Error> for Error {
+    fn from(e: openapi::models::model_409_error::Model409Error) -> Self {
+        match e.code {
+            openapi::models::model_409_error::Code::HasNoPasskeys => Error::UserHasNoPasskeys,
+            _ => Error::Other(e.error),
+        }
+    }
+}
+
+impl From<openapi::models::model_500_error::Model500Error> for Error {
+    fn from(_: openapi::models::model_500_error::Model500Error) -> Self {
+        Error::InternalServerError
+    }
+}
---
+++ src/passage_flex.rs
@@ -0,0 +1,167 @@
+use crate::Error;
+use models::{AppInfo, UserInfo};
+use openapi::apis::configuration::Configuration;
+use openapi::apis::{apps_api, authenticate_api, transactions_api, user_devices_api, users_api};
+use openapi::models;
+
+pub struct PassageFlex {
+    app_id: String,
+    configuration: Configuration,
+}
+
+const SERVER_URL: &str = "https://api.passage.id";
+
+impl PassageFlex {
+    /// Initialize the PassageFlex client
+    pub fn new(app_id: String, api_key: String) -> Self {
+        let mut configuration = Configuration::new();
+        // Use the api_key as the bearer access token
+        configuration.bearer_access_token = Some(api_key);
+        // Set the Passage-Version header to the version of the crate
+        let mut headers = reqwest::header::HeaderMap::with_capacity(1);
+        headers.insert(
+            "Passage-Version",
+            reqwest::header::HeaderValue::from_static(concat!(
+                "passage-flex-rust ",
+                env!("CARGO_PKG_VERSION")
+            )),
+        );
+        configuration.client = reqwest::Client::builder()
+            .default_headers(headers)
+            .build()
+            .expect("Failed to create reqwest client for Passage");
+
+        let mut client = Self {
+            app_id,
+            configuration,
+        };
+        // Set the default server URL
+        client.set_server_url(SERVER_URL.to_string());
+
+        client
+    }
+
+    fn set_server_url(&mut self, server_url: String) {
+        // Use the app_id and server_url to set the base_path
+        self.configuration.base_path = format!("{}/v1/apps/{}", server_url, self.app_id);
+    }
+
+    /// Get the app information
+    pub async fn get_app(&self) -> Result<Box<AppInfo>, Error> {
+        apps_api::get_app(&self.configuration)
+            .await
+            .map(|response| response.app)
+            .map_err(Into::into)
+    }
+
+    /// Create a transaction to start a user's registration process
+    pub async fn create_register_transaction(
+        &self,
+        external_id: String,
+        passkey_display_name: String,
+    ) -> Result<String, Error> {
+        transactions_api::create_register_transaction(
+            &self.configuration,
+            models::CreateTransactionRegisterRequest {
+                external_id,
+                passkey_display_name,
+            },
+        )
+        .await
+        .map(|response| response.transaction_id)
+        .map_err(Into::into)
+    }
+
+    /// Create a transaction to start a user's authentication process
+    pub async fn create_authenticate_transaction(
+        &self,
+        external_id: String,
+    ) -> Result<String, Error> {
+        transactions_api::create_authenticate_transaction(
+            &self.configuration,
+            models::CreateTransactionAuthenticateRequest { external_id },
+        )
+        .await
+        .map(|response| response.transaction_id)
+        .map_err(Into::into)
+    }
+
+    /// Verify the nonce received from a WebAuthn registration or authentication ceremony
+    pub async fn verify_nonce(&self, nonce: String) -> Result<String, Error> {
+        authenticate_api::authenticate_verify_nonce(&self.configuration, models::Nonce { nonce })
+            .await
+            .map(|response| response.external_id)
+            .map_err(Into::into)
+    }
+
+    /// Get a user by their external ID
+    pub async fn get_user(&self, external_id: String) -> Result<Box<UserInfo>, Error> {
+        let users = users_api::list_paginated_users(
+            &self.configuration,
+            Some(1),
+            Some(1),
+            None,
+            None,
+            Some(&external_id),
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+        )
+        .await
+        .map(|response| response.users);
+
+        match users {
+            Ok(mut users) => match users.len() {
+                0 => Err(Error::UserNotFound),
+                1 => {
+                    let user = users.remove(0);
+                    self.get_user_by_id(user.id).await
+                }
+                _ => Err(Error::Other("Multiple users found".to_string())),
+            },
+            Err(e) => Err(e.into()),
+        }
+    }
+
+    /// Get a user's devices by their external ID
+    pub async fn get_devices(
+        &self,
+        external_id: String,
+    ) -> Result<Vec<models::WebAuthnDevices>, Error> {
+        let user = match self.get_user(external_id).await {
+            Ok(user) => user,
+            Err(e) => return Err(e),
+        };
+
+        user_devices_api::list_user_devices(&self.configuration, &user.id)
+            .await
+            .map(|response| response.devices)
+            .map_err(Into::into)
+    }
+
+    /// Revoke a user's device by their external ID and the device ID
+    pub async fn revoke_device(&self, external_id: String, device_id: String) -> Result<(), Error> {
+        let user = match self.get_user(external_id).await {
+            Ok(user) => user,
+            Err(e) => return Err(e),
+        };
+
+        user_devices_api::delete_user_devices(&self.configuration, &user.id, &device_id)
+            .await
+            .map_err(Into::into)
+    }
+
+    /// Get a user by their user ID
+    pub async fn get_user_by_id(&self, user_id: String) -> Result<Box<UserInfo>, Error> {
+        users_api::get_user(&self.configuration, &user_id)
+            .await
+            .map(|response| response.user)
+            .map_err(Into::into)
+    }
+}
+
+#[cfg(test)]
+mod test;

2cb9f512ae52f02e4b3b8f8e4c8c52f7b0ee178f
Bert Ramirez <13988480+bertrmz@users.noreply.github.com>
2025-03-11 09:56:32 -0500

    fix: sync codegen output & pin codegen version (#42)
    * pin the openapi-generator image to last used minor version
    
    * set to last release used
    
    * re-run codegen

--- generate.sh
+++ generate.sh
@@ -64,7 +64,7 @@ process_directory() {
 }
 
 # Run codegen
-docker run --rm -v "${PWD}:/local" -u $(id -u) openapitools/openapi-generator-cli:latest generate \
+docker run --rm -v "${PWD}:/local" -u $(id -u) openapitools/openapi-generator-cli:v7.10.0 generate \
   -i "/local/$file" \
   -g rust \
   -o /local/openapi \
